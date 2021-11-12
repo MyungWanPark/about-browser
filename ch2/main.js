108a0f56febcfd810b372e493b0d1c7e182a6414
MyungWanPark <mywanpark@gmail.com>
2021-11-12 13:41:21 +0900

    JS: make function first as function needed

--- ch2/main.js
+++ ch2/main.js
@@ -1,8 +1,64 @@
 "use strict";
 
 const field = document.querySelector(".game__field");
+const gameBtn = document.querySelector(".game__button");
+const timer = document.querySelector(".game__timer");
+const score = document.querySelector(".game__score");
 const fieldRect = field.getBoundingClientRect();
 const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 5;
+
+let started = false;
+
+gameBtn.addEventListener("click", () => {
+  if (started) {
+    stopGame();
+  } else {
+    startGame();
+  }
+  started = !started;
+});
+
+function stopGame() {}
+
+function startGame() {
+  initGame();
+  showStopBtn();
+  showTimerAndScore();
+  startTimer();
+}
+
+function startTimer() {
+  let time_left = GAME_DURATION_SEC;
+  showRemainingTime(time_left);
+
+  const timer = setInterval(() => {
+    if (time_left <= 0) {
+      clearInterval(timer);
+      return;
+    }
+    showRemainingTime(--time_left);
+  }, 1000);
+}
+
+function showRemainingTime(sec) {
+  const minute = Math.floor(sec / 60);
+  const second = sec % 60;
+  timer.innerText = `${minute}:${second}`;
+}
+
+function showTimerAndScore() {
+  timer.style.visibility = "visible";
+  score.style.visibility = "visible";
+}
+
+function showStopBtn() {
+  const icon = document.querySelector(".fa-play");
+  icon.classList.add("fa-stop");
+  icon.classList.remove("fa-play");
+}
 
 function randomNumber(min, max) {
   return Math.random() * (max - min) + min;
@@ -28,8 +84,8 @@ function addItem(className, num, srcPath) {
 }
 
 function initGame() {
-  addItem("carrot", 5, "img/carrot.png");
-  addItem("bug", 5, "img/bug.png");
+  field.innerHTML = "";
+  score.innerText = CARROT_COUNT;
+  addItem("carrot", CARROT_COUNT, "img/carrot.png");
+  addItem("bug", BUG_COUNT, "img/bug.png");
 }
-
-initGame();
